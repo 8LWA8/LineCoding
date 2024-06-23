5cd66a4abd05fc53caa93808569bc2e07aac4b0a
Hunter599 <brunokunnen@gmail.com>
2024-06-23 13:49:04 -0300

    Adicao do decode

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pylab as plt
 
 # Define your data (e.g., binary stream)
-data = [0, 0, 0, 1, 1, 1, 1, 0,0,0]  # Example data
+data = [0, 0, 0, 1, 1, 1, 1, 0, 0, 0]  # Example data
 
 # Map data to 4D-PAM5 symbols (you'll need a lookup table)
 # For simplicity, assume each symbol lasts for T seconds
@@ -14,11 +14,11 @@ time = np.arange(0, len(data) * T, T)
 # Generate PAM5 waveform (replace with your mapping logic)
 pam5_waveform = []  # List of voltage levels
 
+# ------------------------------------------- CODING -----------------------------------------------_#
+
 for i in range(0, len(data), 2):
     # Extract two bits
     bits = data[i:i + 2]
-
-    print(bits)
     
     # Map to PAM5 voltage level
     if bits == [0, 0]:
@@ -32,12 +32,42 @@ for i in range(0, len(data), 2):
     else:
         pam5_waveform.append(0)   # probably useless    
 
+# ------------------------------------------- CODING ------------------------------------------------#
 
 # Create time axis
 time = np.arange(len(pam5_waveform))
 
 # Upsample PAM5 levels to create a square waveform
 square_waveform = np.repeat(pam5_waveform, int(1 / T))
+
+# ------------------------------------------ DECODING -----------------------------------------------#
+outBits = []
+for i in range(len(pam5_waveform)):
+    if pam5_waveform[i] == -2:
+        outBits.append(0)
+        outBits.append(0)
+    elif pam5_waveform[i] == -1:
+        outBits.append(1)
+        outBits.append(0)
+    elif pam5_waveform[i] == 1:
+        outBits.append(0)
+        outBits.append(1)
+    elif pam5_waveform[i] == 2:
+        outBits.append(1)
+        outBits.append(1)
+    elif pam5_waveform[i] == 0:
+        print("No signal")  #probably useless
+    else:
+        print("Invalid")
+
+# ------------------------------------------ DECODING -----------------------------------------------#
+
+#Just checking if the signal are the same
+"""
+print(outBits)
+print(data)
+"""
+
 
 # Plot the waveform
 plt.plot(time, square_waveform, drawstyle="steps-pre", marker="o", linestyle="-", color="b", label="PAM5 Signal")
@@ -48,3 +78,4 @@ plt.title("PAM5 Square Waveform")
 plt.grid(True)
 plt.legend()
 plt.show()
+
