a185eacf868872652f61079e7d0dd2354c16d834
8LWA8 <112349482+8LWA8@users.noreply.github.com>
2024-07-01 19:09:03 -0300

    Cliente funcionando corretamente

--- cliente.py
+++ cliente.py
@@ -6,8 +6,8 @@ import socket
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
-'''
 import base64
+'''
 import string
 
 #Bibliotecas de interface
